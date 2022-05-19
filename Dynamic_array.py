f8a4c66fc8d51ddf529a219b6bfd342e61145c00
wengwenchao123 <61003827+wengwenchao123@users.noreply.github.com>
2022-05-19 11:42:36 +0800

    Add files via upload

--- Dynamic_array.py
+++ Dynamic_array.py
@@ -3,10 +3,10 @@ from math import ceil
 
 class DynamicArray(object):
     def __init__(self, capacity=1, grow_factor=0.2):
-        self.length = 0    # Actual number of elements in dynamic array
+        self.length = 0  # Actual number of elements in dynamic array
         self.capacity = capacity  # Initialize chunk of memory size to 1
-        self.grow_factor = grow_factor   # grow_factor is set to 1.2
-        self.chunk = [None] * self.capacity  # Allocate initialized blocks
+        self.grow_factor = grow_factor  # grow_factor is set to 1.2
+        self.chunk = [None] * self.capacity  # Allocate initialized memory blocks
 
     def add_element(self, element):
         if element is not None and type(element) != int:
@@ -18,7 +18,11 @@ class DynamicArray(object):
         self.chunk[self.length] = element
         self.length += 1
 
-    def __eq__(self, other):    # Check A is equality to B
+    def __iter__(self):
+        return Iterator(self.chunk, self.length)
+
+    def __eq__(self, other):  # Check A is equality to B
+        """for eq() implementation"""
         assert type(other) == DynamicArray
         if self.length != other.length:
             return False
@@ -28,23 +32,70 @@ class DynamicArray(object):
                     return False
         return True
 
-    def __str__(self):     # String serialization
+    def __str__(self):  # String serialization
+        """for str() implementation"""
         return str(self.chunk[:self.length])
 
 
+class Iterator(object):
+    """ An iterator object of DynamicArray. """
+
+    def __init__(self, chunk, length):
+        self.chunk = chunk
+        self.length = length
+        self.element_index = -1
+
+    def __iter__(self):
+        """ Implement iter(self). """
+        return self
+
+    def __next__(self):
+        """ Implement next(self). """
+        self.element_index += 1
+        if self.element_index >= self.length:
+            raise StopIteration()
+        return self.chunk[self.element_index]
+
+
+def copy(lst):
+    """ Copy an LST
+    
+    :param lst: The given DynamicArray
+    """
+    assert type(lst) is DynamicArray
+    if lst.length == lst.capacity:
+        res = DynamicArray(ceil(lst.capacity*(1+lst.grow_factor)))
+        res.length = lst.length
+    else:
+        res = DynamicArray(lst.capacity)
+        res.capacity = lst.capacity
+        res.length = lst.length
+    for i in range(lst.length):
+        res.chunk[i] = lst.chunk[i]
+    return res
+
+
 def cons(lst, element=None):
+    """ Add an element at the end of the array.
+
+    :param lst: The given DynamicArray
+    :param element: The given element
+    """
     if type(lst) is int and type(element) == DynamicArray:
         res = cons(element, lst)
     else:
         assert type(lst) == DynamicArray
-        res = DynamicArray()
-        for i in range(lst.length):
-            res.add_element(lst.chunk[i])
+        res = copy(lst)
         res.add_element(element)
     return res
 
 
 def remove(lst, value=None):
+    """ Remove an element of array at specified position.
+
+    :param lst: The given DynamicArray
+    :param value: Index of the array.
+    """
     assert type(lst) == DynamicArray
     if value < 0 or value >= lst.length:
         return 'The location accessed is not in the dynamic array'
@@ -56,11 +107,21 @@ def remove(lst, value=None):
 
 
 def length(lst):
+    """ Return the length of array.
+
+    :param lst: The given DynamicArray
+    """
     assert type(lst) == DynamicArray
     return lst.length
 
 
 def member(lst, v):
+    """ Determines whether the given value is a member of the array.
+
+    :param lst: The given DynamicArray
+    :param v: The given value.
+    :return: Value is member if return True, else not.
+    """
     assert type(lst) == DynamicArray
     if v not in lst.chunk[:lst.length]:
         return False
@@ -69,14 +130,22 @@ def member(lst, v):
 
 
 def reverse(lst):
+    """ Reverse the array.
+
+    :param lst: The given DynamicArray
+    """
     assert type(lst) == DynamicArray
-    res = DynamicArray()
+    res = DynamicArray(len(lst.chunk))
     for i in range(lst.length):
-        res.add_element(lst.chunk[lst.length-i-1])
+        res.add_element(lst.chunk[lst.length - i - 1])
     return res
 
 
 def to_list(lst):
+    """ Transform the array to a list.
+
+    :param lst: The given DynamicArray
+    """
     assert type(lst) == DynamicArray
     res = []
     for i in range(lst.length):
@@ -85,6 +154,10 @@ def to_list(lst):
 
 
 def from_list(lst):
+    """ Add elements from a list to the empty array
+
+    :param lst: The given list
+    """
     assert type(lst) == list
     res = DynamicArray()
     for i in lst:
@@ -93,6 +166,12 @@ def from_list(lst):
 
 
 def find(lst, function):
+    """ Search the array for elements that match the function
+
+    :param lst: The given DynamicArray
+    :param function: The given function
+    :return: Ture or Flase
+    """
     assert callable(function)
     for i in lst.chunk:
         if function(i):
@@ -101,8 +180,14 @@ def find(lst, function):
 
 
 def filter(lst, function):
+    """ Filter the array by specific function
+
+    :param lst: The given DynamicArray
+    :param function: The given function
+    :return: The transformed array
+    """
     assert callable(function)
-    res = DynamicArray()
+    res = DynamicArray(len(lst.chunk))
     for i in range(lst.length):
         if function(lst.chunk[i]):
             res.add_element(lst.chunk[i])
@@ -110,15 +195,29 @@ def filter(lst, function):
 
 
 def map(lst, function):
+    """ Applied function to every item of instances of DynamicArray,
+    yielding the results.
+
+    :param lst: The given DynamicArray
+    :param function: The given function
+    :return: The transformed array
+    """
     assert type(lst) == DynamicArray
     assert callable(function)
-    res = DynamicArray()
+    res = DynamicArray(len(lst.chunk))
     for i in range(lst.length):
         res.add_element(function(lst.chunk[i]))
     return res
 
 
 def reduce(lst, function, initial_state):
+    """  Apply function of two arguments cumulatively to the items of the
+         array, from left to right, to reduce the array to a single value.
+
+    :param lst: The given DynamicArray
+    :param function: The given function
+    :param initial_state: the optional initializer
+    """
     assert type(lst) == DynamicArray
     assert callable(function)
     assert type(initial_state) == int
@@ -130,27 +229,25 @@ def reduce(lst, function, initial_state):
 
 
 def iterator(lst):
-    length = lst.length
-    res = lst
-    index = 0
+    """ An iterator object of DynamicArray.
 
-    def foo():
-        nonlocal res
-        nonlocal length
-        nonlocal index
-        if (index >= length) | (res is None):
-            raise StopIteration
-        tmp = res.chunk[index]
-        index = index+1
-        return tmp
-    return foo
+    :param lst: The given DynamicArray
+    """
+    return iter(lst)
 
 
 def empty():
+    """ An empty array"""
     return DynamicArray
 
 
 def concat(lst1, lst2):
+    """ Concatenate arrays 1 and 2
+
+    :param lst1: The given DynamicArray1
+    :param lst2: The given DynamicArray2
+    :return: Concatenated array
+    """
     assert type(lst1) == DynamicArray
     res = DynamicArray()
     for i in range(lst1.length):
@@ -161,12 +258,17 @@ def concat(lst1, lst2):
 
 
 def eq(lst1, lst2):
+    """ Check whether two arrays are equal
+
+    :return: Ture or Flase
+    """
     assert type(lst1) == DynamicArray
     assert type(lst2) == DynamicArray
     return lst1.__eq__(lst2)
 
 
 def str1(lst):
+    """ Returns the string version of an array element"""
     assert type(lst) == DynamicArray
     res = str(lst.chunk[:lst.length])
     return res
--- Dynamic_array_test.py
+++ Dynamic_array_test.py
@@ -6,6 +6,43 @@ import hypothesis.strategies as st
 
 
 class TestDynamicArray(unittest.TestCase):
+    def test_api(self):
+        empty = DynamicArray()
+
+        l1 = cons(cons(empty, 1), None)
+        l2 = cons(cons(empty, None), 1)
+        # TODO: conj to add elements to the end
+        self.assertEqual(str(empty), "[]")
+        self.assertEqual(str(l1), "[1, None]")
+        self.assertEqual(str(l2), "[None, 1]")
+        self.assertNotEqual(empty, l1)
+        self.assertNotEqual(empty, l2)
+        self.assertNotEqual(l1, l2)
+        self.assertEqual(l1, cons(cons(empty, 1), None))
+        self.assertEqual(length(empty), 0)
+        self.assertEqual(length(l1), 2)
+        self.assertEqual(length(l2), 2)
+        self.assertEqual(str(remove(l1, 0)), "[None]")
+        self.assertEqual(str(remove(l1, 1)), "[1]")
+        self.assertFalse(member(empty, None))
+        self.assertTrue(member(l1, None))
+        self.assertTrue(member(l1, 1))
+        self.assertFalse(member(l1, 2))
+        self.assertEqual(l1, reverse(l2))
+        self.assertEqual(to_list(l1), [1, None])
+        self.assertEqual(l1, from_list([1, None]))
+        self.assertEqual(concat(l1, l2), from_list([1, None, None, 1]))
+        buf = []
+        for e in l1:
+            buf.append(e)
+        self.assertEqual(buf, [1, None])
+        lst = to_list(l1) + to_list(l2)
+        for e in l1:
+            lst.remove(e)
+        for e in l2:
+            lst.remove(e)
+        self.assertEqual(lst, [])
+
     # Add a new element
     def test_Add_a_new_element(self):
         empty1 = DynamicArray()
@@ -87,7 +124,7 @@ class TestDynamicArray(unittest.TestCase):
         try:
             get_next = iterator(lst)
             while True:
-                tmp.append(get_next())
+                tmp.append(next(get_next))
         except StopIteration:
             pass
         self.assertEqual(x, tmp)
@@ -133,5 +170,4 @@ class TestDynamicArray(unittest.TestCase):
         lst1 = from_list(a)
         lst2 = from_list(b)
         lst3 = from_list(c)
-        self.assertEqual(concat(concat(lst1, lst2), lst3),
-                         concat(lst1, concat(lst2, lst3)))
+        self.assertEqual(concat(concat(lst1, lst2), lst3), concat(lst1, concat(lst2, lst3)))
